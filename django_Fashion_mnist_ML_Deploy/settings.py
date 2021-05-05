123d19ddfc27f13666afcd1c078b3f7792bd5837
Marwenv1 <marwen.tlili@esprit.tn>
2021-05-05 02:30:48 +0100

    fin

--- django_Fashion_mnist_ML_Deploy/settings.py
+++ django_Fashion_mnist_ML_Deploy/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -122,4 +123,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+import django_heroku
+
 django_heroku.settings(locals())
